323ead4541ae4f1880bbffbcf02640f697feb335
olegprokhorov2001 <olegprokhorov2001@gmail.com>
2022-04-01 09:42:44 +0300

    Добавьте файлы проекта.

---
+++ Zadanie 1/Kvadratnoe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zadanie_1
+{
+    internal class Kvadratnoe
+    {
+        internal void Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Zadanie 1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            kvadratnoe_uravnenie taskWindow = new kvadratnoe_uravnenie();
+            taskWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            lineynoe_uravnenie taskWindow = new lineynoe_uravnenie ();
+                taskWindow.Show();
+                this.Close();
+            }
+    }
+}
